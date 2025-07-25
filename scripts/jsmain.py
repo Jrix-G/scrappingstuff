fae88eac3d81d6272ef582fe36b220e3ac992443
Jason <jrixweb@gmail.com>
2025-07-26 01:20:19 +0200

    +

--- scripts/jsmain.py
+++ scripts/jsmain.py
@@ -1,25 +1,39 @@
+from tkinter import simpledialog
 from pytrends.request import TrendReq
 import json
+import tkinter as tk
 from datetime import datetime
+from trends_show import afficher_graphique
 
-pytrends = TrendReq(hl='fr-FR', tz=360)
 
-pytrends.build_payload(kw_list=['messi'], timeframe='today 1-m', geo='FR')
+def importDataFromTrends(name: str):
+    pytrends = TrendReq(hl='fr-FR', tz=360)
+    pytrends.build_payload(kw_list=[name], timeframe='today 12-m', geo='FR')
+    data = pytrends.interest_over_time()
 
-data = pytrends.interest_over_time()
+    if 'isPartial' in data.columns:
+        data = data.drop(columns=['isPartial'])
 
-if 'isPartial' in data.columns:
-    data = data.drop(columns=['isPartial'])
+    result = [
+        {
+            "date": index.strftime("%Y-%m-%d %H:%M:%S"),
+            name: int(row[name])
+        }
+        for index, row in data.iterrows()
+    ]
 
-result = [
-    {
-        "date": index.strftime("%Y-%m-%d %H:%M:%S"),
-        "messi": int(row["messi"])
-    }
-    for index, row in data.iterrows()
-]
+    with open(f"./graphs/trends_{name}.json", "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
 
-with open("trends_messi.json", "w", encoding="utf-8") as f:
-    json.dump(result, f, ensure_ascii=False, indent=4)
+    print(f"Tout bien enregistré dans ./graphs/trends_{name}.json")
 
-print(f"✅ {len(result)} entrées enregistrées dans trends_messi.json")
+
+root = tk.Tk()
+root.withdraw()
+
+mot_cle = simpledialog.askstring("Mot-clé Google Trends", "Entrez le graphe à importer:")
+
+if mot_cle:
+    importDataFromTrends(mot_cle)
+    
+afficher_graphique(mot_cle)
--- scripts/trends_show.py
+++ scripts/trends_show.py
@@ -1,22 +1,30 @@
 import json
+import os
 import matplotlib.pyplot as plt
 from datetime import datetime
+import tkinter as tk
+from tkinter import simpledialog
 
-with open("trends_messi.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
+def afficher_graphique(mot_cle):
+    my_file = f"./graphs/trends_{mot_cle}.json"
+    
+    if os.path.exists(my_file):
+        with open(my_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
 
-dates = [datetime.strptime(entry["date"], "%Y-%m-%d %H:%M:%S") for entry in data]
-values = [entry["messi"] for entry in data]
+        dates = [datetime.strptime(entry["date"], "%Y-%m-%d %H:%M:%S") for entry in data]
+        values = [entry[mot_cle] for entry in data]
 
-plt.figure(figsize=(12, 6))
-plt.plot(dates, values, marker="o", linestyle="--", color="blue", label="Messi")
-
-plt.title("Intérêt pour 'Messi' sur Google Trends", fontsize=14)
-plt.xlabel("Date et heure", fontsize=12)
-plt.ylabel("Intensité de recherche (0 à 100)", fontsize=12)
-plt.grid(True)
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
-
-plt.show()
+        plt.figure(figsize=(12, 6))
+        plt.plot(dates, values, marker="o", linestyle="--", color="blue", label=mot_cle)
+        plt.title(f"Intérêt pour '{mot_cle}' sur Google Trends", fontsize=14)
+        plt.xlabel("Date et heure", fontsize=12)
+        plt.ylabel("Intensité de recherche (de 0 à 100)", fontsize=14)
+        plt.grid(True)
+        plt.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+        
+        #Fin du if os    
+    
