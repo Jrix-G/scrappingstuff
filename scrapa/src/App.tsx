d0ba8454745c442b6357945664af864744416bdf
Jason <jrixweb@gmail.com>
2025-08-06 01:22:06 +0200

    Tandor base project

--- scrapa/src/App.tsx
+++ scrapa/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- scrapa/src/Pages/Home.tsx
+++ scrapa/src/Pages/Home.tsx
@@ -1,42 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import TrendGraph from './TrendGraph';
+import React, { useState } from 'react';
+import '../Styles/home.css';
+import Navbar from './Navbar';
 
-type Dataset = {
-  title: string;
-  data: { date: string; value: number }[];
-  valueKey: string;
-};
+// @ts-ignore
+import coloredbackground from '../Assets/homepage/backendground.png';
+// @ts-ignore
+import group from '../Assets/homepage/groupimage.png';
 
 const Home = () => {
-  const [datasets, setDatasets] = useState<Dataset[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3001/api/graphs')
-      .then((res) => res.json())
-      .then(async (fileList: string[]) => {
-        const datasets = await Promise.all(
-          fileList.map(async (file) => {
-            const res = await fetch(`http://localhost:3001/graphs/${file}`);
-            const rawData = await res.json();
-            const valueKey = Object.keys(rawData[0]).find((k) => k !== 'date') || '';
-            const title = file.replace('trends_', '').replace('.json', '');
-            const data = rawData.map((item: any) => ({
-              date: item.date.split(' ')[0],
-              value: item[valueKey],
-            }));
-            return { title, data, valueKey: 'value' };
-          })
-        );
-        setDatasets(datasets);
-      });
-  }, []);
-
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ textAlign: 'center' }}>Graphiques dynamiques</h1>
-      {datasets.map(({ title, data, valueKey }) => (
-        <TrendGraph key={title} title={title} data={data} valueKey={valueKey} />
-      ))}
+    <div>
+      <Navbar />
+      <div className='home-main-page-div'>
+        <div className='home-first-in-page-div'>
+          <div className='home-text-div-position'>
+            <h1 className='home-h1-main-text'>Affordable Position <span className='colored-span'>Tracking</span> For <span className='colored-span'>Startups</span> And <span className='colored-span'>Businesses</span></h1>
+            <div className='home-div-p-text'>
+              <p>Affordable SEO tracking for startups: Monitor keyword rankings, get actionable insights, and optimize your content effortlessly.</p>
+            </div>
+            <button className='home-button-get-started'>Get Started Now</button>
+          </div>
+          <div>
+            <img className='home-word-volume-png' src={group} alt="word group" />
+          </div>
+        </div>
+        <img className='home-colored-background' src={coloredbackground} alt="coloredbackground" />
+      </div>
     </div>
   );
 };
